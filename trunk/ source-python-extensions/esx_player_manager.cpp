0c02dce64e56fc8422f51dc93920763a55993d2f
thinkerroad@gmail.com <thinkerroad@gmail.com@6d1f2fe0-fee4-11dd-8259-6defae454439>
2009-02-20 00:28:33 +0000

    * Initial Import

---
+++ trunk/ source-python-extensions/esx_player_manager.cpp
@@ -0,0 +1,39 @@
+//=============================================================================//
+// Copyright: Deniz Sezen
+//=============================================================================//
+
+#include "esx_player_manager.h"
+
+CPlayerManager* gPlayerManager;
+
+//=============================================================================//
+// >> Constructor
+//=============================================================================//
+CPlayerManager::CPlayerManager( IVEngineServer *pEngine, IPlayerInfoManager* pInfo )
+{
+	m_Engine = pEngine;
+	m_Info = pInfo;
+}
+
+//=============================================================================//
+// >> Returns an edict_t to a player
+//=============================================================================//
+edict_t* CPlayerManager::GetPlayerByUserID(int userid)
+{
+	for(int i = 0; i < m_Info->GetGlobalVars()->maxClients; i++)
+	{
+		edict_t* player = m_Engine->PEntityOfEntIndex(i);
+
+		if(!player || player->IsFree())
+			continue;
+
+		if(m_Engine->GetPlayerUserId(player) == -1)
+			continue;
+
+		if(m_Engine->GetPlayerUserId(player) == userid)
+			return player;
+	}
+
+	return NULL;
+}
+
---
+++ trunk/ source-python-extensions/esx_signature_manager.h
@@ -0,0 +1,26 @@
+//=============================================================================//
+// Copyright: Deniz Sezen
+//		Defines this modules python methods
+//=============================================================================//
+
+#ifndef ESX_SIGNATURE_MANAGER_H
+#define ESX_SIGNATURE_MANAGER_H
+
+#include "esx_scanner.h"
+
+//=============================================================================
+// >> Signature Scanner class
+//=============================================================================
+class CSigger
+{
+	private:
+		void* m_addr;
+	public:
+		CSigger( void* addr );
+		void* findFunction( const char* sig, int length );
+};
+
+extern CSigger* gSigger;
+
+#endif
+

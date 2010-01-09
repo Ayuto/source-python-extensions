2feb566fe182228bc2d6af7029ae6b3db5e14f73
Michael Barr <michaeljohnbarr@users.noreply.github.com>
2010-01-09 19:30:19 +0000

    ./_libs/games/cstrike.py: * Removed unused imports. (import es, from os import name as platform). * Added return values to all functions so that scripters can check of successful completion. * Added comments to all methods within the functions. * Ordered the function calls to return None or False as checks progressed in order to improve readability of the logic.

--- _libs/games/cstrike.py
+++ _libs/games/cstrike.py
@@ -7,12 +7,6 @@
 # Imports
 #================================================================================
 
-# Python imports
-from os import name as platform
-
-# Eventscripts imports.
-import es
-
 # SPE imports
 import spe
 
@@ -24,66 +18,90 @@ import spe
 # Respawns a player
 #================================================================================
 def respawn( userid ):
-
-    # Get player instance
+    # Get the player instance
     pPlayer = spe.getPlayer(int(userid))
 
-    if pPlayer:
-        # Respawn the player
-        spe.call("Respawn", pPlayer)
+    # Make sure the player instance is valid
+    if not pPlayer:
+        # Return False since the player instance was not valid
+        return False
+
+    # Respawn the player
+    spe.call("Respawn", pPlayer)
+
+    return True
 
 #================================================================================
 # Switches a player's team without killing them.
 #================================================================================    
 def switchTeam( userid, team_index ):
-
-    # Get player instance
+    # Get the player instance
     pPlayer = spe.getPlayer(int(userid))
 
-    if pPlayer:
+    # Is the player instance valid?
+    if not pPlayer:
+        # Return False since the player instance was not valid
+        return False
 
-        # Switch their team
-        spe.call("ChangeTeam", pPlayer, int(team_index))
+    # Switch their team
+    spe.call("ChangeTeam", pPlayer, int(team_index))
+
+    return True
 
 #================================================================================
 # Returns an instance to a player's active weapon (the weapon they are currently
 #   holding).
 #================================================================================   
 def getActiveWeapon( userid ):
-
-    # Get player instance
+    # Get the player instance
     pPlayer = spe.getPlayer(int(userid))
 
-    if pPlayer:
+    # Is the player instance valid?
+    if not pPlayer:
+        # Return None since the player instance was not valid
+        return None
 
-        # Call and return player's active weapon
-        return spe.call("GetActiveWeapon", pPlayer)
+    # Call and return player's active weapon
+    return spe.call("GetActiveWeapon", pPlayer)
 
 #================================================================================
 # Gives a player a named item.
 #================================================================================
 def giveNamedItem( userid, item_name ):
-
+    # Get the player instance
     pPlayer = spe.getPlayer(int(userid))
 
-    if pPlayer:
+    # Is the player instance valid?
+    if not pPlayer:
+        # Return False since the player instance was not valid
+        return False
 
-        # Give the player the item
-        spe.call('GiveNamedItem', pPlayer, str(item_name))
+    # Give the player the item
+    spe.call('GiveNamedItem', pPlayer, str(item_name))
+
+    return True
 
 #================================================================================
 # If the player owns the weapon_instance entity, it forces them to drop it.
 #================================================================================
 def dropWeapon( userid, weapon_name ):
-
+    # Get the player instance
     pPlayer = spe.getPlayer(int(userid))
 
+    # Is the player instance valid?
     if not pPlayer:
-        return
+        # Return False since the player instance was not valid
+        return False
 
+    # Get the weapon instance
     weapon_instance = spe.ownsWeapon( userid, weapon_name )
 
-    if weapon_instance:
+    # Is the weapon instance valid?
+    if not weapon_instance:
+        # Return False since the weapon instance was not valid
+        return False
 
-        # Make them drop it
-        spe.call('DropWeapon', pPlayer, weapon_instance)
+    # Make them drop it
+    spe.call('DropWeapon', pPlayer, weapon_instance)
+
+    return True
